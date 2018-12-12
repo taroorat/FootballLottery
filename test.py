95e1a1529660f1f9df43fe4b10bb16d5d2982a7d
chenrq <2548316823@qq.com>
2018-12-12 16:45:19 +0800

    赔率结果

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-
 import pandas as pd
-df = pd.read_csv("data/home-level.csv",sep=' ')
 
-df.columns=["AGVH","AGVD","AGVA","LEVEL"]
-df["SUM"]=df["AGVH"]+df["AGVD"]+df["AGVA"]
-df["AGVH"]=(df["AGVH"]/df["SUM"])
-df["AGVD"]=(df["AGVD"]/df["SUM"])
-df["AGVA"]=(df["AGVA"]/df["SUM"])
-df=df.round(2)
-print(df)
-df.to_csv('data/home-level-answer.csv')
+def WinLevelLose(bunko):
+    df = pd.read_csv("data/home-%s.csv"%bunko,sep=' ')
+
+    df.columns=["AGVH","AGVD","AGVA","LEVEL"]
+    df["SUM"]=df["AGVH"]+df["AGVD"]+df["AGVA"]
+    df["AGVH"]=(df["AGVH"]/df["SUM"])
+    df["AGVD"]=(df["AGVD"]/df["SUM"])
+    df["AGVA"]=(df["AGVA"]/df["SUM"])
+    df=df.round(2)
+    print(df)
+    df.to_csv('data/home-%s-answer.csv'%bunko)
+
+WinLevelLose("level")
