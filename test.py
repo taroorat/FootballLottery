99938316f88bb5d0a8e70ab4b2e18389b00a7159
chenrq <2548316823@qq.com>
2018-12-12 16:37:48 +0800

    赔率结果

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ df = pd.read_csv("data/home-level.csv",sep=' ')
 
 df.columns=["AGVH","AGVD","AGVA","LEVEL"]
 df["SUM"]=df["AGVH"]+df["AGVD"]+df["AGVA"]
-df["AGVH"]=df["AGVH"]/df["SUM"]
-df["AGVD"]=df["AGVD"]/df["SUM"]
-df["AGVA"]=df["AGVA"]/df["SUM"]
+df["AGVH"]=(df["AGVH"]/df["SUM"])
+df["AGVD"]=(df["AGVD"]/df["SUM"])
+df["AGVA"]=(df["AGVA"]/df["SUM"])
 df=df.round(2)
 print(df)
 df.to_csv('data/home-level-answer.csv')
